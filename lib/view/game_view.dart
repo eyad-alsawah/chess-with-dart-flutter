c18362b9953dca7cfa77e1ad558ec81c341f2357
eyad.alsawah <eyad.alsawah@nasca-sy.com>
2023-06-07 20:12:03 +0300

    rotated pawn promotion dialog for black and made dialog undismissable by clicking outside of it

--- lib/view/game_view.dart
+++ lib/view/game_view.dart
@@ -7,7 +7,6 @@ import 'package:chess/utils/image_assets.dart';
 import 'package:chess/utils/sound_assets.dart';
 import 'package:flutter/material.dart';
 
-import '../controller/game_controller.dart' hide Files;
 import 'package:chess/model/model.dart';
 
 enum PlayingAs { white, black }
@@ -53,87 +52,90 @@ class _ChessBoardState extends State<ChessBoard> {
         return await showDialog(
             useRootNavigator: true,
             context: context,
-            barrierDismissible: true,
+            barrierDismissible: false,
             builder: (dialogContext) {
               return AlertDialog(
                 contentPadding: EdgeInsets.zero,
                 shape: const RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(12))),
-                content: Container(
-                  padding: const EdgeInsets.all(10),
-                  decoration: const BoxDecoration(
-                    color: Color(0xFFB58863),
-                    borderRadius: BorderRadius.all(Radius.circular(12)),
-                  ),
-                  height: 100,
-                  width: 100,
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.of(dialogContext, rootNavigator: true)
-                                .pop(Pieces.rook);
-                          },
-                          child: Image.asset(
-                            playingTurn == PlayingTurn.white
-                                ? whiteCastle
-                                : blackCastle,
+                content: Transform.rotate(
+                  angle: playingTurn == PlayingTurn.white ? 0 : pi,
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFB58863),
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                    ),
+                    height: 100,
+                    width: 100,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.of(dialogContext, rootNavigator: true)
+                                  .pop(Pieces.rook);
+                            },
+                            child: Image.asset(
+                              playingTurn == PlayingTurn.white
+                                  ? whiteCastle
+                                  : blackCastle,
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        width: 2,
-                        color: Colors.black,
-                      ),
-                      Expanded(
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.of(dialogContext, rootNavigator: true)
-                                .pop(Pieces.knight);
-                          },
-                          child: Image.asset(
-                            playingTurn == PlayingTurn.white
-                                ? whiteKnight
-                                : blackKnight,
+                        Container(
+                          width: 2,
+                          color: Colors.black,
+                        ),
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.of(dialogContext, rootNavigator: true)
+                                  .pop(Pieces.knight);
+                            },
+                            child: Image.asset(
+                              playingTurn == PlayingTurn.white
+                                  ? whiteKnight
+                                  : blackKnight,
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        width: 2,
-                        color: Colors.black,
-                      ),
-                      Expanded(
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.of(dialogContext, rootNavigator: true)
-                                .pop(Pieces.bishop);
-                          },
-                          child: Image.asset(
-                            playingTurn == PlayingTurn.white
-                                ? whiteBishop
-                                : blackBishop,
+                        Container(
+                          width: 2,
+                          color: Colors.black,
+                        ),
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.of(dialogContext, rootNavigator: true)
+                                  .pop(Pieces.bishop);
+                            },
+                            child: Image.asset(
+                              playingTurn == PlayingTurn.white
+                                  ? whiteBishop
+                                  : blackBishop,
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        width: 2,
-                        color: Colors.black,
-                      ),
-                      Expanded(
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.of(dialogContext, rootNavigator: true)
-                                .pop(Pieces.queen);
-                          },
-                          child: Image.asset(
-                            playingTurn == PlayingTurn.white
-                                ? whiteQueen
-                                : blackQueen,
+                        Container(
+                          width: 2,
+                          color: Colors.black,
+                        ),
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.of(dialogContext, rootNavigator: true)
+                                  .pop(Pieces.queen);
+                            },
+                            child: Image.asset(
+                              playingTurn == PlayingTurn.white
+                                  ? whiteQueen
+                                  : blackQueen,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               );
