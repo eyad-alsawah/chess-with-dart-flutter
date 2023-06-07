04e1c99e670f50d1a0d11877dc8038a194aa61f7
eyad.alsawah <eyad.alsawah@nasca-sy.com>
2023-06-07 18:38:24 +0300

    extracting typedefs into seperate file within controller folder

--- lib/controller/game_controller.dart
+++ lib/controller/game_controller.dart
@@ -1,22 +1,7 @@
 import 'dart:async';
 
 import 'package:chess/controller/enums.dart';
-
-//-------------------typedefs--------------------
-typedef OnVictory = void Function(VictoryType victoryType);
-typedef OnDraw = void Function(DrawType drawType);
-typedef OnPlayingTurnChanged = void Function(PlayingTurn playingTurn);
-typedef OnPieceSelected = void Function(
-    List<int> highlightedLegalMovesIndices, int selectedPieceIndex);
-typedef OnCastling = void Function(
-    CastlingType castlingType, PlayingTurn playingTurn);
-typedef OnPieceMoved = void Function(int from, int to);
-typedef OnError = void Function(Error error, String errorString);
-typedef OnPawnPromoted = void Function(
-    int promotedPieceIndex, Pieces promotedTo);
-typedef OnSelectPromotionType = Future<Pieces> Function(
-    PlayingTurn playingTurn);
-typedef OnEnPassant = void Function(int capturedPawnIndex);
+import 'package:chess/controller/typedefs.dart';
 
 //--------------Main Game Controller-------------------
 class Chess {
---
+++ lib/controller/typedefs.dart
@@ -0,0 +1,16 @@
+import 'package:chess/controller/enums.dart';
+
+typedef OnVictory = void Function(VictoryType victoryType);
+typedef OnDraw = void Function(DrawType drawType);
+typedef OnPlayingTurnChanged = void Function(PlayingTurn playingTurn);
+typedef OnPieceSelected = void Function(
+    List<int> highlightedLegalMovesIndices, int selectedPieceIndex);
+typedef OnCastling = void Function(
+    CastlingType castlingType, PlayingTurn playingTurn);
+typedef OnPieceMoved = void Function(int from, int to);
+typedef OnError = void Function(Error error, String errorString);
+typedef OnPawnPromoted = void Function(
+    int promotedPieceIndex, Pieces promotedTo);
+typedef OnSelectPromotionType = Future<Pieces> Function(
+    PlayingTurn playingTurn);
+typedef OnEnPassant = void Function(int capturedPawnIndex);
